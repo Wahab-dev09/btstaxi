23d72962a5c994141e7b73c6398e8c7a3a4efd79
wahab-dev09 <wahabzsec@gmail.com>
2026-03-06 11:40:34 +0500

    Code Formatted

--- app/Components/ReviewsCarousel.tsx
+++ app/Components/ReviewsCarousel.tsx
@@ -20,17 +20,12 @@ export default function ReviewsCarousel() {
     useEffect(() => {
         const fetchReviews = async () => {
             try {
-                const res = await fetch(
-                    "https://my.api.mockaroo.com/reviews.json?key=520c4880"
-                )
+                const res = await fetch("https://my.api.mockaroo.com/reviews.json?key=520c4880")
                 const data = await res.json()
                 setReviews(data)
             } catch (error) {
-            } finally {
-                setLoading(false)
-            }
+            } finally {  setLoading(false) }
         }
-
         fetchReviews()
     }, [])
 
@@ -49,10 +44,7 @@ export default function ReviewsCarousel() {
         return (
             <div className="flex gap-4 overflow-hidden justify-center">
                 {[1, 2, 3].map((i) => (
-                    <div
-                        key={i}
-                        className="shrink-0 w-[90%] max-w-100 h-65 bg-neutral-300 rounded-2xl animate-pulse"
-                    />
+                    <div key={i} className="shrink-0 w-[90%] max-w-100 h-65 bg-neutral-300 rounded-2xl animate-pulse"/>
                 ))}
             </div>
         )
@@ -60,26 +52,15 @@ export default function ReviewsCarousel() {
     return (
         <div className="relative">
             {/* Arrows */}
-            <button
-                onClick={() => scroll("left")}
-                  aria-label="Scroll left"
-                className="absolute left-2 top-1/2 -translate-y-1/2 bg-white border border-white cursor-pointer text-white rounded-full w-12 h-12 flex justify-center items-center z-10 hover:bg-white/90 transition"
-            >
+            <button onClick={() => scroll("left")} aria-label="Scroll left" className="absolute left-2 top-1/2 -translate-y-1/2 bg-white border border-white cursor-pointer text-white rounded-full w-12 h-12 flex justify-center items-center z-10 hover:bg-white/90 transition">
                 <ChevronLeft pack="filled" size="md" fill="#171717" />
             </button>
-            <button
-                onClick={() => scroll("right")}
-                aria-label="Scroll right"
-                className="absolute right-2 top-1/2 -translate-y-1/2 bg-white border border-white cursor-pointer text-white rounded-full w-12 h-12 flex justify-center items-center z-10 hover:bg-white/90 transition"
-            >
+            <button onClick={() => scroll("right")} aria-label="Scroll right" className="absolute right-2 top-1/2 -translate-y-1/2 bg-white border border-white cursor-pointer text-white rounded-full w-12 h-12 flex justify-center items-center z-10 hover:bg-white/90 transition">
                 <ChevronRight pack="filled" size="md" fill="#171717" />
             </button>
 
             <div className="relative overflow-x-hidden">
-                <div
-                    ref={containerRef}
-                    className="flex scrollbar-hidden gap-4 overflow-x-auto scroll-smooth snap-x snap-mandatory"
-                >
+                <div ref={containerRef} className="flex scrollbar-hidden gap-4 overflow-x-auto scroll-smooth snap-x snap-mandatory">
                     {Array.isArray(reviews) ? (
                         reviews.map((item) => (
                             <ReviewCard key={item.id} item={item} />
